1993df6c9f3ba2c194e529166d2f855fa81592f3
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-02-23 02:55:14 +0000

    Update SelectShapeCommand.java

--- src/Command/SelectShapeCommand.java
+++ src/Command/SelectShapeCommand.java
@@ -4,15 +4,15 @@ package Command;
 import java.io.IOException;
 
 import view.interfaces.PaintCanvasBase;
-
+import ClickHandler.point;
 public class SelectShapeCommand implements Icommand{
 	
 	SelectedShapeList selectedShape;
 	ShapeList list;
-	ClickHandler.point point;
+	point point;
 	
 
-	public SelectShapeCommand(SelectedShapeList selectedShape, ShapeList list, ClickHandler.point point) {
+	public SelectShapeCommand(SelectedShapeList selectedShape, ShapeList list, point point) {
 		super();
 		this.selectedShape = selectedShape;
 		this.list = list;
