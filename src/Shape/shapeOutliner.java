7e67bbcd3e226ea1880559bfb043043f5d922ccf
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-04 16:18:09 +0000

    Update shapeOutliner.java

--- src/Shape/shapeOutliner.java
+++ src/Shape/shapeOutliner.java
@@ -11,23 +11,30 @@ public class shapeOutliner extends shapeOutline{
 		super(outline);
 		// TODO Auto-generated constructor stub
 	}
+	@Override
 	public void Draw(PaintCanvasBase canvas) {
+		
 		shapeProperties prop=getOutline();
 		Graphics2D graphics2d = canvas.getGraphics2D();
+		if(outline.ShapeName().equals(ShapeType.GROUP)) {
+			graphics2d.drawRect(prop.getX(), prop.getY(), prop.getWidth(), prop.getHeight());
+			return;
+			
+		}
 		
-		if(prop.getType().equals(ShapeType.RECTANGLE)) {
+		if(outline.ShapeName().equals(ShapeType.RECTANGLE)) {
 		graphics2d.drawRect(prop.getX(), prop.getY(), prop.getWidth(), prop.getHeight());
-		}else if(prop.getType().equals(ShapeType.ELLIPSE)){
+		}else if(outline.ShapeName().equals(ShapeType.ELLIPSE)){
 		graphics2d.drawOval(prop.getX(), prop.getY(), prop.getWidth(), prop.getHeight());
 			
-		}else {
+		}
+		else if(outline.ShapeName().equals(ShapeType.TRIANGLE)){
 			int x[]=new int[3];
 			int y[]=new int[3];
 			
 			x[0]=prop.getX();
 			x[1]=prop.getX()+prop.getWidth();
 			x[2]=prop.getX();
-			
 			y[0]=prop.getY();
 			y[1]=prop.getY()+prop.getHeight();
 			y[2]=prop.getY()+prop.getHeight();
