80105964f9e04e1187484573325a14da999eaeeb
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-05 11:54:36 +0000

    Update Ellipse.java

--- src/Shape/Ellipse.java
+++ src/Shape/Ellipse.java
@@ -9,6 +9,12 @@ import model.interfaces.IApplicationState;
 import view.interfaces.PaintCanvasBase;
 import java.util.*;
 
+import Command.ShapeList;
+import Stategy.ImoveStrategy;
+import Stategy.move;
+import Stategy.negativeDisplacement;
+import Stategy.positiveDisplacement;
+
 public class Ellipse implements Ishape {
 	private shapeProperties properties;
 	
@@ -18,14 +24,10 @@ public class Ellipse implements Ishape {
 	
 	
 
-	public void setId(String id) {
-		Id = id;
-	}
-
+	
 	public Ellipse(shapeProperties properties) {
 		super();
 		this.properties = properties;
-		
 		Id=uuid.toString();
 	}
 
@@ -81,6 +83,58 @@ public class Ellipse implements Ishape {
 		Ishape that=(Ishape)o;
 		
 		return  getId().equals(that.getId());
+	}
+
+	
+	@Override
+	public void positivedisplacement(int dx,int dy){
+		// TODO Auto-generated method stub
+		move move=new move(dx,dy,this);
+		ImoveStrategy moveStrategy=new positiveDisplacement();
+		move.displace(moveStrategy);
+		
+	}
+
+	
+	@Override
+	public void negativedisplacement(int dx,int dy) {
+		// TODO Auto-generated method stub
+		move move=new move(dx,dy,this);
+		ImoveStrategy moveStrategy=new negativeDisplacement();
+		move.displace(moveStrategy);
+		
+	}
+
+	
+	@Override
+	public Ishape copy(Ishape shape,int x) {
+		// TODO Auto-generated method stub
+		shapeProperties prop=shape.getProperties();
+		int a=prop.getX()+x;
+		int b=prop.getY()-x;
+		int w=prop.getWidth();
+		int h=prop.getHeight();
+		ShapeShadingType shade=prop.getShade();
+		ShapeType shapetype=prop.getType();
+		ShapeColor shapecolor=prop.getColor();
+		shapeProperties prop2=new shapeProperties(a, b, w, h, shade, shapecolor, shapetype);
+		IshapeFactory factory=new CreateShapeFactory();
+		Ishape copy=factory.CreateShape2(prop2,shapetype);
+		return copy;
+	}
+
+	@Override
+	public void delete(Ishape shape, ShapeList shapelist) {
+		// TODO Auto-generated method stub
+		shapelist.RemoveShape(shape);
+		
+	}
+
+	@Override
+	public void addtolist(Ishape shape, ShapeList shapelist) {
+		// TODO Auto-generated method stub
+		shapelist.AddShape(shape);
+		
 	}
 	
 
