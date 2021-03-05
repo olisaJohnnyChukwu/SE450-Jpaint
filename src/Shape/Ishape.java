19eea56a7106970493097da7ad6248c541cc7d11
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-05 11:59:52 +0000

    Update Ishape.java

--- src/Shape/Ishape.java
+++ src/Shape/Ishape.java
@@ -1,21 +1,22 @@
 package Shape;
 
+import Command.ShapeList;
 import model.ShapeType;
 import view.interfaces.PaintCanvasBase;
 
 public interface Ishape {
 	
-
-	
-	
 	public void Draw(PaintCanvasBase canvas);
 	public ShapeType ShapeName();
 	public shapeProperties getProperties();
 	public String getId();
-	public void  setId(String id);
 	public void setProperties(shapeProperties properties);
 	public boolean equals(Object o);
-	
+	public void negativedisplacement(int dx, int dy);
+	public void positivedisplacement(int dx, int dy);
+	public Ishape copy(Ishape shape, int x);
+	public void delete(Ishape shape,ShapeList shapelist);
+	public void addtolist(Ishape shape,ShapeList shapelist);
 	
 
 }
