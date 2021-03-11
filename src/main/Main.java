3b8e8edfd23dce49d3f61d1a4b0db3dea4a2909d
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-11 15:05:05 +0000

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,22 +12,33 @@ import view.interfaces.PaintCanvasBase;
 import view.interfaces.IUiModule;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 import ClickHandler.clickHandler;
 import Command.CopyShapeList;
 import Command.SelectedShapeList;
 import Command.ShapeList;
+import Command.copylistrepo;
+import Command.repository;
+import Command.selectshaperepo;
+import Command.shapelistrepo;
+import Shape.Ishape;
 
 public class Main {
     public static void main(String[] args){
+    	
         PaintCanvasBase paintCanvas = new PaintCanvas();
+        repository shaperepository= new shapelistrepo(new ArrayList<Ishape>(), paintCanvas);
+        repository selectshaperepository= new selectshaperepo(new ArrayList<Ishape>(), paintCanvas);
+        repository coyshaperepository= new copylistrepo(new ArrayList<Ishape>(), paintCanvas);
+        
         ShapeList shapelist= new ShapeList(paintCanvas);
         SelectedShapeList selectedShape=new SelectedShapeList();
         CopyShapeList copyList=new CopyShapeList();
         IGuiWindow guiWindow = new GuiWindow(paintCanvas);
         IUiModule uiModule = new Gui(guiWindow);
         ApplicationState appState = new ApplicationState(uiModule);
-        IJPaintController controller = new JPaintController(uiModule, appState,selectedShape,copyList,shapelist);
+        IJPaintController controller = new JPaintController(uiModule, appState,selectedShape,copyList,shapelist, shaperepository,selectshaperepository,coyshaperepository);
         controller.setup();
 
         // For example purposes only; remove all lines below from your final project.
@@ -38,27 +49,8 @@ public class Main {
             e.printStackTrace();
         }
         
-        paintCanvas.addMouseListener(new clickHandler(paintCanvas,shapelist,appState,selectedShape));
-        Graphics2D graphics2d = paintCanvas.getGraphics2D();
-        graphics2d.drawOval(10, 15, 100, 200);
-        /*graphics2d.setColor(Color.WHITE);
-        graphics2d.fillRect(0, 0, paintCanvas.getWidth(), paintCanvas.getHeight());*/
-        
-
-        // Filled in rectangle
-      /*  Graphics2D graphics2d = paintCanvas.getGraphics2D();
-        graphics2d.setColor(Color.GREEN);
-        graphics2d.fillRect(12, 13, 200, 400);
-
-        // Outlined rectangle
-        graphics2d.setStroke(new BasicStroke(5));
-        graphics2d.setColor(Color.BLUE);
-        graphics2d.drawRect(12, 13, 200, 400);
-
-        // Selected Shape
-        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
-        graphics2d.setStroke(stroke);
-        graphics2d.setColor(Color.BLACK);
-        graphics2d.drawRect(7, 8, 210, 410);*/
+        paintCanvas.addMouseListener(new clickHandler(paintCanvas,shapelist,appState,selectedShape, shaperepository,selectshaperepository));
+       
+       
     }
 }
