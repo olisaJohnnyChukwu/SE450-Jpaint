bfe4a434b231a8abb9b35a626a96e01ef62d067a
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-12 14:28:09 +0000

    Update pasteCommand.java

--- src/Command/pasteCommand.java
+++ src/Command/pasteCommand.java
@@ -4,39 +4,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import Shape.CreateShapeFactory;
-import Shape.Ishape;
-import Shape.IshapeFactory;
-import Shape.shapeProperties;
-import Stategy.ImoveStrategy;
-import Stategy.move;
-import Stategy.positiveDisplacement;
-import model.ShapeColor;
-import model.ShapeShadingType;
-import model.ShapeType;
+import Shape.*;
 import model.interfaces.IApplicationState;
-import model.persistence.ApplicationState;
+
+
 
 public class pasteCommand implements Icommand, IUndoable {
-	int num;
-	CopyShapeList copyList;
-	ShapeList shapelist;
+	
 	List<Ishape> clipboard;
 	IApplicationState applicationState;
+	repository shaperepository;
+	repository coyshaperepository;
 	int x;
 
-	
-	
-	 
-	public pasteCommand(int num, CopyShapeList copyList, ShapeList shapelist,IApplicationState applicationState,int x) {
+	public pasteCommand(IApplicationState applicationState,int x,repository shaperepository,repository coyshaperepository) {
 		super();
-		this.num = num;
-		this.copyList = copyList;
-		this.shapelist = shapelist;
 		clipboard=new ArrayList<>();
 		this.applicationState=applicationState;
 		this.x=x;
-		
+		this.shaperepository=shaperepository;
+		this.coyshaperepository=coyshaperepository;
 		
 	}
 
@@ -44,10 +31,13 @@ public class pasteCommand implements Icommand, IUndoable {
 	public void undo() {
 		// TODO Auto-generated method stub
 		for(Ishape s:clipboard) {
-			shapelist.traverse(s);
+			
+			if(shaperepository.contains(s)) {
+				shaperepository.removeshape(s);
+			}
 		}
 		
-		shapelist.redraw();
+		shaperepository.redraw();
 		
 		
 		
@@ -60,22 +50,22 @@ public class pasteCommand implements Icommand, IUndoable {
 		
 		
 		for(Ishape s:clipboard) {
-			shapelist.AddShape(s);
+			shaperepository.addshape(s);
 		}
-		
+		shaperepository.redraw();
 	}
 
 	@Override
 	public void run() throws IOException {
 		
-		for(Ishape shape:copyList.getCopyList()) {
+		for(Ishape shape:coyshaperepository.list()) {
 			
 			Ishape copy=shape.copy(shape,x);
 			System.out.println(copy.equals(shape));
 			clipboard.add(copy);
-			shapelist.AddShape(copy);
+			shaperepository.addshape(copy);
 			}
-		System.out.println(shapelist.getList());
+		System.out.println(shaperepository.list());
 		
 		CommandHistory.add(this);
 	}
