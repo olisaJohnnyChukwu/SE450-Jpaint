454a3c50968eb9d177c8896cc6a0bd322491d334
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-12 14:29:41 +0000

    Update SelectShapeCommand.java

--- src/Command/SelectShapeCommand.java
+++ src/Command/SelectShapeCommand.java
@@ -3,19 +3,22 @@ package Command;
 
 import java.io.IOException;
 
+import Shape.Ishape;
+import Shape.shapeProperties;
 import view.interfaces.PaintCanvasBase;
 import ClickHandler.point;
+
 public class SelectShapeCommand implements Icommand{
 	
-	SelectedShapeList selectedShape;
-	ShapeList list;
-	point point;
 	
+	point point;
+	repository shaperepository;
+	repository selectshaperepository;
 
-	public SelectShapeCommand(SelectedShapeList selectedShape, ShapeList list, point point) {
+	public SelectShapeCommand(repository shaperepository, repository selectshaperepository, point point) {
 		super();
-		this.selectedShape = selectedShape;
-		this.list = list;
+		this. shaperepository =  shaperepository;
+		this.selectshaperepository = selectshaperepository;
 		this.point = point;
 		
 	}
@@ -24,12 +27,42 @@ public class SelectShapeCommand implements Icommand{
 	@Override
 	public void run() throws IOException {
 		// TODO Auto-generated method stub
-		selectedShape.clear();
-		selectedShape.inRange(point, list);
+		
+		
+		selectshaperepository.clear();
+		inRange(point);
+		
 		
 		
 	}
-	
+	public void inRange(point point) {
+		int Ax1=point.getX1();
+		int Ax2=point.getX1()+point.getWidth();
+		int Ay1=point.getY1();
+		int Ay2=point.getY1()+point.getHeight();
+		
+		
+		
+		
+		for(Ishape shape:shaperepository.list()) {
+			shapeProperties properties=shape.getProperties();
+			
+	if(Ax1<properties.getX()+properties.getWidth()&&Ax2>properties.getX()&&Ay1<properties.getY()+properties.getHeight()&&Ay2>properties.getY()) {
+			if(!selectshaperepository.contains(shape)) {
+				selectshaperepository.addshape(shape);
+			   }
+					}
+				
+				
+			
+		}
+		
+		
+		System.out.println("Number of selected shapes"+selectshaperepository.size());
+		
+		
+			
+		}
 
 	
 
