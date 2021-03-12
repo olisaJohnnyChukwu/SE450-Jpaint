eec662facd8694e789f4147481baabbd95a850cc
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-12 14:28:51 +0000

    Update MoveCommand.java

--- src/Command/MoveCommand.java
+++ src/Command/MoveCommand.java
@@ -3,30 +3,28 @@ package Command;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import Shape.Ishape;
-import Shape.shapeProperties;
-import Stategy.ImoveStrategy;
-import Stategy.move;
-import Stategy.negativeDisplacement;
-import Stategy.positiveDisplacement;
+import Shape.*;
+
 
 public class MoveCommand implements Icommand, IUndoable {
 	
-	SelectedShapeList selectedShape;
-	ShapeList list;
+	
 	ArrayList<Ishape> clipboard;
 	int dx;
 	int dy;
+	repository shaperepository;
+	repository selectshaperepository;
 	
 	
 
-	public MoveCommand(SelectedShapeList selectedShape, ShapeList list, int dx, int dy) {
+	public MoveCommand( int dx, int dy,repository shaperepository,repository selectshaperepository) {
 		super();
-		this.selectedShape = selectedShape;
-		this.list = list;
+		
 		this.dx = dx;
 		this.dy = dy;
 		clipboard=new ArrayList<>();
+		this.shaperepository=shaperepository;
+		this.selectshaperepository=selectshaperepository;
 	}
 
 	
@@ -34,16 +32,14 @@ public class MoveCommand implements Icommand, IUndoable {
 	@Override
 	public void undo() {
 		// TODO Auto-generated method stub
-
-		
-		
 		for(Ishape shape:clipboard) {
-			if(selectedShape.getSelectedshapelist().contains(shape)) {
+			if(selectshaperepository.contains(shape)) {
 				shape.negativedisplacement(dx, dy);
 			}
 			
 		}
-		list.redraw();
+		shaperepository.redraw();
+
 		
 	}
 
@@ -53,26 +49,26 @@ public class MoveCommand implements Icommand, IUndoable {
 		
 
 		for(Ishape shape:clipboard) {
-			if(selectedShape.getSelectedshapelist().contains(shape)) {
+			if(selectshaperepository.contains(shape)) {
 				shape.positivedisplacement(dx, dy);
 			}
 			
 		}
-		list.redraw();
+		shaperepository.redraw();
 		
 		
 	}
 
 	@Override
 	public void run() throws IOException {
-		for(Ishape shape:list.getList()) {
-			if(selectedShape.getSelectedshapelist().contains(shape)) {
+		for(Ishape shape:shaperepository.list()) {
+			if(selectshaperepository.contains(shape)) {
 				shape.positivedisplacement(dx, dy);
 				clipboard.add(shape);
 			}
 			
 		}
-		list.redraw();
+		shaperepository.redraw();
 		CommandHistory.add(this);
 		
 		
@@ -82,3 +78,4 @@ public class MoveCommand implements Icommand, IUndoable {
 	}
 
 }
+
