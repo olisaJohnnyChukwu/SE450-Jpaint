1a65551747ee850ee04aeb03fa44ebfa9c7d2b24
olisaJohnnyChukwu <77973254+olisaJohnnyChukwu@users.noreply.github.com>
2021-03-12 14:27:12 +0000

    Update ungroupCommand.java

--- src/Command/ungroupCommand.java
+++ src/Command/ungroupCommand.java
@@ -1,24 +1,33 @@
 package Command;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.io.*;
 import java.util.*;
 
 import Shape.Ishape;
 import Shape.shapeGroup;
+import view.interfaces.PaintCanvasBase;
 
 public class ungroupCommand implements Icommand, IUndoable {
-	SelectedShapeList selectedShape;
-	ShapeList shapelist;
+	
 	ArrayList<Ishape> clipboard;
+	repository shaperepository;
+	repository selectshaperepository;
+	PaintCanvasBase canvas;
+	public ungroupCommand(repository shaperepository,
+			repository selectshaperepository,PaintCanvasBase canvas) {
+		super();
+		
+		clipboard=new ArrayList<Ishape>();
+		this.shaperepository = shaperepository;
+		this.selectshaperepository = selectshaperepository;
+		this.canvas=canvas;
+	}
 	
 	
 
-	public ungroupCommand(SelectedShapeList selectedShape, ShapeList shapelist) {
-		super();
-		this.selectedShape = selectedShape;
-		this.shapelist = shapelist;
-		clipboard=new ArrayList<Ishape>();
-	}
+	
 
 	@Override
 	public void undo() {
@@ -27,10 +36,9 @@ public class ungroupCommand implements Icommand, IUndoable {
 		for(Ishape sh:clipboard) {
 			shapeGroup s=(shapeGroup)sh;
 			for(Ishape a:s.getGroup()) {
-				shapelist.RemoveShape(a);
+				shaperepository.removeshape(a);
 			}
-			shapelist.addGroup(sh);
-			
+			addGroup(sh, shaperepository);
 		}
 		
 
@@ -40,9 +48,7 @@ public class ungroupCommand implements Icommand, IUndoable {
 	public void redo() {
 		// TODO Auto-generated method stub
 		for(Ishape a:clipboard) {
-			//a.addtolist(a, shapelist);
-			a.ungroup(a, shapelist,clipboard);
-			
+			a.ungroup(a,shaperepository,clipboard);
 		}
 		
 	}
@@ -50,12 +56,21 @@ public class ungroupCommand implements Icommand, IUndoable {
 	@Override
 	public void run() throws IOException {
 		// TODO Auto-generated method stub
-		for(Ishape a:selectedShape.getSelectedshapelist()) {
-			a.ungroup(a, shapelist,clipboard);
+		for(Ishape a:selectshaperepository.list()) {
+			a.ungroup(a, shaperepository,clipboard);
 		}
-		
 		CommandHistory.add(this);
 		
 	}
+	
+	public void addGroup(Ishape shape,repository shaperepository) {
+		Graphics2D graphics2d = canvas.getGraphics2D();
+		graphics2d.setColor(Color.WHITE);
+        graphics2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        shaperepository.addshape(shape);
+		shape.Draw(canvas);
+		
+	}
+	
 
 }
